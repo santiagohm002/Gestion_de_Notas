1cc58827b40663cbb57bcaf42daa093c452f745a
Santiago Hernandez <santiago.hernandezm@hotmail.com>
2024-06-05 21:18:04 -0500

    Se mejora visualmente el FormAdmin con Submenus para cada items desplegable

--- Gestión de Notas/Gestión de Notas/FormPortalAdmin.cs
+++ Gestión de Notas/Gestión de Notas/FormPortalAdmin.cs
@@ -16,34 +16,131 @@ namespace Gestión_de_Notas
         public FormPortalAdmin()
         {
             InitializeComponent();
+            customizeDesing();
         }
+
+        private void customizeDesing()
+        {
+            panelStudentSubmenu.Visible = false;
+            panelTeacherSubmenu.Visible = false;
+            panelAdminSubmenu.Visible = false;
+            panelGradesSubmenu.Visible = false;
+        }
+
+        private void hideSubMenu()
+        {
+            if (panelStudentSubmenu.Visible == true)
+                panelStudentSubmenu.Visible = false;
+            if (panelTeacherSubmenu.Visible == true)
+                panelTeacherSubmenu.Visible=false;
+            if (panelAdminSubmenu.Visible == true)
+                panelAdminSubmenu.Visible = false;
+            if (panelGradesSubmenu.Visible == true)
+                panelGradesSubmenu.Visible = false;
+        }
+
+        private void showSubMenu(Panel subMenu)
+        {
+            if(subMenu.Visible == false)
+            {
+                hideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+            {
+                subMenu.Visible = false;
+            }
+        }
+
         private void btn_Student_Click(object sender, EventArgs e)
         {
-            openChildform(new FormStudent());
+            showSubMenu(panelStudentSubmenu);
         }
+        private void btn_RegistStud_Click(object sender, EventArgs e)
+        {
+            openChildform(new FormStudent());
+            hideSubMenu();
+        }
+
+        private void btn_AssigStud_Click(object sender, EventArgs e)
+        {
+            //openChildform(new FormAssigStud());
+            hideSubMenu();
+        }
+
+        private void btn_GrafRoom_Click(object sender, EventArgs e)
+        {
+            //openChildform(new FormGrafRoom());
+            hideSubMenu();
+        }
+
+        private void btn_GrafSub_Click(object sender, EventArgs e)
+        {
+            //openChildform(new FormGrafSub());
+            hideSubMenu();
+        }
+
         private void btn_Teacher_Click(object sender, EventArgs e)
         {
-            openChildform(new FormTeacher());
+            showSubMenu(panelTeacherSubmenu);
         }
-        private void btn_AssignTeach_Click(object sender, EventArgs e)
-        {
-            openChildform(new FormAssigTeach());
-        }
-        private void btn_Period_Click(object sender, EventArgs e)
-        {
 
-        }
-        private void btn_Notes_Click(object sender, EventArgs e)
+        private void btn_RegistTeach_Click(object sender, EventArgs e)
         {
+            //openChildform(new FormRegistTeach());
+            hideSubMenu();
         }
+
+        private void btn_AssigTeach_Click(object sender, EventArgs e)
+        {
+            //openChildform(new FormAssigTeach());
+            hideSubMenu();
+        }
+
         private void btn_Grades_Click(object sender, EventArgs e)
         {
-            openChildform(new FormGrades());
+            showSubMenu(panelGradesSubmenu);
         }
+
+        private void btn_UpdGrad_Click(object sender, EventArgs e)
+        {
+            //openChildform(new FormUpdGrad());
+            hideSubMenu();
+        }
+
+        private void btn_UpdRoom_Click(object sender, EventArgs e)
+        {
+            //openChildform(new FormUpdRoom());
+            hideSubMenu();
+        }
+
+        private void btn_Admin_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelAdminSubmenu);
+        }
+        private void btn_RegistAdmin_Click(object sender, EventArgs e)
+        {
+            //openChildform(new FormRegistAdmin());
+            hideSubMenu();
+        }
+
+        private void btn_Periods_Click(object sender, EventArgs e)
+        {
+            //openChildform(new FormPeriods());
+            hideSubMenu();
+        }
+
+        private void btn_Notes_Click(object sender, EventArgs e)
+        {
+            //openChildform(new FormNotesAdmin());
+            hideSubMenu();
+        }
+
         private void btn_Close_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
+
         private Form activeForm = null;
         private void openChildform(Form childForm)
         {
